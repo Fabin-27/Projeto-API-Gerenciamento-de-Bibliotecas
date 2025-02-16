b3a487e9b79cd242569779bcf2b64668e32d1931
Fabin-27 <fabiolourenco063@gmail.com>
2025-02-16 10:58:38 -0300

    First Commit

---
+++ src/main/java/com/Biblioteca/Gerenciamento_Bibliotecas/GerenciamentoBibliotecasApplication.java
@@ -0,0 +1,13 @@
+package com.Biblioteca.Gerenciamento_Bibliotecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoBibliotecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoBibliotecasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Biblioteca/Gerenciamento_Bibliotecas/GerenciamentoBibliotecasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Biblioteca.Gerenciamento_Bibliotecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoBibliotecasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
